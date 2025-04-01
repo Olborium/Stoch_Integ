bc8714d826fa00b93dafd0d0f3d62debfb3d1e0e
Andrey Shkerin <149336823+Olborium@users.noreply.github.com>
2025-04-01 16:28:26 -0400

    Update decay_rate.py

--- field_theory/decay_rate.py
+++ field_theory/decay_rate.py
@@ -1,11 +1,10 @@
+# Read the decay times, calculate the decay rate from the slope of the survival probability.
+# Make figure 7 from the paper
 #%%
 import os
 import re
-import sys
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-from scipy.interpolate import interp1d 
 from scipy.optimize import curve_fit
 
 plt.rcParams.update({
@@ -81,7 +80,7 @@ for j in range(M):
     for i in range(t_points):
         Psurv[j,i] = (Times[j]>t_l[j][i]).sum()/len(Times[j])
 
-def Gamma1(i, tMin=500, log_P_max=2):
+def Gamma1(i, tMin=200, log_P_max=2):
     log_Psurv = np.log(Psurv[i])   
     iMin = int(tMin*t_points/TimeOfSim)
     iMax = np.where((-log_Psurv<log_P_max))[0][-1]
@@ -105,14 +104,15 @@ t = np.linspace(1e-4, 2e2, 10000)
 fig, ax = plt.subplots(figsize=(6,4))
 ax.tick_params(axis='both',which='both',right=True,top=True,direction='in',labelsize=16)
 ax.axhline(y=1, linestyle='dashdot', linewidth=0.75, color='black')
-ax.set_xlabel(r'$\hat{\eta}$',fontsize=18)
-ax.set_ylabel(r'$A^{(sim)}/A_E$',fontsize=18)
+ax.set_xlabel(r'$\eta$',fontsize=18)
+ax.set_ylabel(r'$\Gamma^{\rm{(sim)}}/\Gamma_{\rm{E}}$',fontsize=18)
 ax.xaxis.grid(True,linestyle='dashed',linewidth=0.5,color='grey')
 ax.yaxis.grid(True,linestyle='dashed',linewidth=0.5,color='grey')
 plt.xscale('log')
 plt.ylim((0, 1.05))
 plt.xlim((7e-4,70))
 ax.plot(t, Langer(t), 'red', linestyle='dashed', linewidth=1, dashes=[6,2])
+plt.text(5,0.85,r'$\mathcal{T}=0.1$',fontsize=18)
 ax.errorbar(eta_list, ff_list, ff_list_e, color='black', fmt='o', ms=5, markerfacecolor='black', elinewidth=1, capsize=2)
 plt.savefig('Decay_rate_full.pdf', bbox_inches='tight')
 plt.show()
@@ -132,11 +132,12 @@ a = params[0]
 b = params[1]
 
 str1 = r'$y=$'+f'{b:.2f}'+r'$x+$'+f'{a:.2f}'
+str1 = r'$y=$'+f'{b:.2f}'+r'$x+$'+f'{a:.2f}'
 t = np.linspace(0.5*eta_list_short[0],2*eta_list_short[-1],1000)
 ts = np.linspace(0.95*eta_list_short[2],2*eta_list_short[-1],1000)
 fig, ax = plt.subplots(figsize=(6,4))
-ax.set_xlabel(r'$\hat{\eta}$',fontsize=18)
-ax.set_ylabel(r'$A^{(sim)}/A_E$',fontsize=18)
+ax.set_xlabel(r'$\eta$',fontsize=18)
+ax.set_ylabel(r'$\Gamma^{\rm{(sim)}}/\Gamma_{\rm{E}}$',fontsize=18)
 ax.tick_params(axis='both',which='both',right=True,top=True,direction='in',labelsize=16)
 ax.xaxis.grid(True,linestyle='dashed',linewidth=0.5,color='grey')
 ax.yaxis.grid(True,linestyle='dashed',linewidth=0.5,color='grey')
@@ -147,12 +148,12 @@ ax.set_xticklabels([None,None,None,None,5,None,None,None,None,10,20,None,None,50
 ax.errorbar(eta_list_short,ff_list_short,ff_list_e_short,color='black', fmt='o', ms=5, markerfacecolor='black', elinewidth=1, capsize=2)
 ax.set_yticks([0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.2,0.3,0.4,0.5,0.6])
 ax.set_yticklabels([0.02,None,None,0.05,None,None,None,None,0.1,0.2,None,None,0.5,None])
-ax.plot(t,Langer(t),color='red',linestyle='dashdot')
-ax.plot(ts,ts**b*10**a,color='blue',linestyle='dashed',linewidth=1)
+ax.plot(t,Langer(t),color='red',linestyle='dashed')
+ax.plot(ts,ts**b*10**a,color='blue',linestyle='solid',linewidth=1)
 plt.xlim((0.8*eta_list_short[0],1.2*eta_list_short[-1]))
 plt.ylim((1.5e-2,7e-1))
 plt.text(2e1,2e-1,str1,fontsize=16,color='blue')
-plt.savefig('Decay_rate_as.pdf', bbox_inches='tight')
+plt.savefig('Decay_rate.pdf', bbox_inches='tight')
 plt.show()
 
 #%%
