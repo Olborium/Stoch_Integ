306d8f3e3f9ad1f894472dd7ddd1b745d4377ce6
Andrey Shkerin <149336823+Olborium@users.noreply.github.com>
2025-04-01 11:39:48 -0400

    Update conv_weak_temp.py

--- field_theory/conv_weak_temp.py
+++ field_theory/conv_weak_temp.py
@@ -3,38 +3,41 @@
 ###########################
 #
 # The script reads momenta power spectra of stochastic solutions computed with different time steps.
-# At each time step, it computes the average and the variance of the effective temperature of long and short Fourier modes.
+# At each time step, it computes the average and variance of the effective temperature of long and short Fourier modes.
 # The linear fit is made in the log-log scale and the rate of convergence to the theory value TEMP is measured.
 
+#%%
 import os
 import re
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 plt.rcParams.update({
     "text.usetex": True,
     "font.family": "serif",
 })
 
-SIZE = float(sys.argv[1])
-N_x = int(sys.argv[2])
-N_SAMPLE = int(sys.argv[3])
-BETA = int(sys.argv[4])
-GAMMA = int(sys.argv[5])
-TEMP = float(sys.argv[6])
-ETA = float(sys.argv[7])
-N = int(sys.argv[8])
-DTs = np.zeros(N)
-Ts = np.zeros(N)
-for i in range(N):
-    DTs[i] = float(sys.argv[i+9])
-    Ts[i] = float(sys.argv[i+9+N])
+#%%
+SIZE = 100
+N_x = 8192
+k_max = 31
+N_SAMPLE = 50
+BETA = 4
+GAMMA = 3
+TEMP = 0.1
+ETA = 1
+SIGN = 1
+
+N = 14
+DTs = np.array([0.005, 0.007, 0.01, 0.02, 0.03, 0.04, 0.05, 0.07, 0.1, 0.2, 0.3, 0.4, 0.5, 0.7])
+Ts = np.array([10.0, 9.8, 10.0, 10.0, 9.9, 10.0, 10.0, 9.8, 10.0, 10.09, 9.9, 10.0, 10.0, 9.8])
+
 DX = SIZE/N_x
 
 k = np.linspace(0, np.pi/DX, N_x//2)
-k_max = int(SIZE/np.pi)
-
+#%%
 TeffLon = np.zeros(N)
 TeffLonE = np.zeros(N)
 TeffSho = np.zeros(N)
@@ -50,7 +53,7 @@ for i in range(N):
 
     DT = DTs[i]
     TIME_SPAN = DT*N_SAMPLE
-    N_min = int(N_SAMPLE*10/ETA/TIME_SPAN)  # give it some time to thermalize
+    N_min = int(N_SAMPLE*20/ETA/TIME_SPAN)  # give it some time to thermalize
 
     str = 'ps_'+f'{ETA:.6f}'+'_'+f'{TEMP:.6f}'+'_'+f'{DT:.6f}'+'_'+f'{BETA}'+'_'+f'{GAMMA}'+'_'+f'{SIGN}'
     for filename in files:
@@ -74,6 +77,7 @@ for i in range(N):
     TeffLonE[i] = np.sqrt(TeffLonE[i])/(SimNo-1)
     TeffShoE[i] = np.sqrt(TeffShoE[i])/(SimNo-1)    
 
+#%% 
 # make a fit:
 
 def f(h,a,b):
@@ -87,12 +91,8 @@ params, cov = curve_fit(f, np.log10(DTs[0:4]), np.log10(np.abs(TeffSho/TEMP-1)[0
 (aS, bS) = (params[0], params[1])
 print(aS,bS)
 
+#%%
 # make a plot:
-
-str =  '_'+f'{ETA:.3f}'+\
-        '_'+f'{TEMP:.2f}'+\
-        '_'+f'{BETA}'+\
-        '_'+f'{GAMMA}'
 
 if bL>0:
     str1 = r'$y=$'+f'{aL:.2f}'+r'$x+$'+f'{np.abs(bL):.2f}'
@@ -104,18 +104,22 @@ else:
     str2 = r'$y=$'+f'{aS:.2f}'+r'$x-$'+f'{np.abs(bS):.2f}'
 h = np.linspace(DTs[0],DTs[-1],100)
 fig, ax = plt.subplots(figsize=(6,4))
-ax.yaxis.grid(True,linestyle='dashed',linewidth=0.5,color='grey')
-ax.xaxis.grid(True,linestyle='dashed',linewidth=0.5,color='grey')
-ax.tick_params(axis='both',which='both',right=True,top=True,direction='in',labelsize=16)
+
 ax.set_xlabel(r'$h$',fontsize=18)
 ax.set_ylabel(r'$\delta_{\mathcal{T}} $',fontsize=18)
 ax.set_xscale('log',base=10)
 ax.set_yscale('log',base=10)
 plt.ylim((1e-5,3e-0))
-ax.plot(h, h**aS*10**bS,'red',linestyle='dashed')
+ax.plot(h, h**aS*10**bS,'tomato',linestyle='dashed')
 ax.plot(h, h**aL*10**bL,'blue',linestyle='dashdot')
-ax.errorbar(DTs, np.abs(TeffSho/TEMP-1), (TeffShoE/TEMP), color='darkred', fmt='o', ms=5, markerfacecolor='black', elinewidth=1, capsize=2)
-ax.errorbar(DTs, np.abs(TeffLon/TEMP-1), (TeffLonE/TEMP), color='darkblue', fmt='D', ms=5, markerfacecolor='black', elinewidth=1, capsize=2)
-plt.text(2.5e-2,2e-1,str2,fontsize=14,color='red')
-plt.text(1e-1,2e-3,str1,fontsize=14,color='blue')
-plt.savefig('Conv_weak_T'+str+'.pdf', bbox_inches='tight')
+ax.errorbar(DTs, np.abs(TeffSho/TEMP-1), 2*TeffShoE/TEMP, color='red', fmt='o', ms=5, markerfacecolor='red', elinewidth=1, capsize=2)
+ax.errorbar(DTs, np.abs(TeffLon/TEMP-1), 2*TeffLonE/TEMP, color='blue', fmt='s', ms=5, markerfacecolor='blue', elinewidth=1, capsize=2)
+plt.text(2.5e-2,2e-1,str2,fontsize=16,color='red')
+plt.text(0.09,2e-3,str1,fontsize=16,color='blue')
+ax.yaxis.grid(True,linestyle='dashed',linewidth=0.5,color='grey')
+ax.xaxis.grid(True,linestyle='dashed',linewidth=0.5,color='grey')
+ax.tick_params(axis='both',which='both',right=True,top=True,direction='in',labelsize=16)
+ax.set_yticks([1e-5,1e-4,1e-3,1e-2,1e-1,1e0])
+ax.set_xticks([0.005,0.01,0.02,0.05,0.1,0.2,0.5])
+ax.set_xticklabels([0.005,0.01,0.02,0.05,0.1,0.2,0.5])
+plt.savefig('Temp43.pdf', bbox_inches='tight')
