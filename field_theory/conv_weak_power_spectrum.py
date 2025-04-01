576b8fa115f599456a682771ca9a5f90aa7a95c7
Andrey Shkerin <149336823+Olborium@users.noreply.github.com>
2025-04-01 16:14:05 -0400

    Update conv_weak_power_spectrum.py

--- field_theory/conv_weak_power_spectrum.py
+++ field_theory/conv_weak_power_spectrum.py
@@ -27,7 +27,7 @@ ETA = 1
 SIGN = 1
 N = 8
 DTs = np.array([0.05, 0.07, 0.1, 0.2, 0.3, 0.4, 0.5, 0.7])
-Ts = np.array([0.5, 0.49, 0.5, 0.6, 0.6, 0.8, 1.0, 1.4])
+Ts = np.array([10.0, 9.8, 10.0, 10.0, 9.9, 10.0, 10.0, 9.8])
 
 DX = SIZE/N_x
 
@@ -59,6 +59,8 @@ def f(h,a,b):
     return a*h + b
 
 #%%
+# read data
+
 PS = []
 PSE = []
 
@@ -66,54 +68,36 @@ GammaList = [2,3]
 
 for w in range(2):
     
-    ps_phi = []
     GAMMA = GammaList[w]
+    PS_av = np.zeros(N)
+    PS_var = np.zeros(N)
 
     for i in range(N):
-        DT = DTs[i]
-        str = 'ps_phi_'+f'{ETA:.6f}'+'_'+f'{TEMP:.6f}'+'_'+f'{DT:.6f}'+'_'+f'{BETA}'+'_'+f'{GAMMA}'+'_'+f'{SIGN}'+'_'
-        pattern = re.compile(rf'^{str}\d+$')
-        for filename in files:
-            if pattern.search(filename):
-                print('loading file ', filename)
-                filepath = os.path.join(path,filename)
-                ps_phi  += [np.loadtxt(filepath)]
-
-    DeltaPS = np.zeros(N)
-    SigmaDeltaPS = np.zeros(N)
-
-    for i in range(N):
-
-        TIME_SPAN = Ts[i]*N_SAMPLE
-        DT = DTs[i]
-        N_min = int(N_SAMPLE*20*ETA/TIME_SPAN) # initial thermalization time excluded
-
-        """ Power spectrum """
-
-        ps_sum = np.zeros((N_SAMPLE))
-        for l in range(N_SAMPLE):
-            for j in range(j_trunc):
-                kj = 2*np.pi*j/SIZE
-                ps_sum[l] += ps_phi[i][l][j]*DX/TEMP
-            ps_sum[l] /= j_trunc
-
-        ps_sum_av = 0
-        for l in np.arange(N_min,N_SAMPLE,1):
-            ps_sum_av += ps_sum[l]
-        ps_sum_av /= (N_SAMPLE-N_min)
-
-        ps_sum_var = 0
-        for l in np.arange(N_min,N_SAMPLE,1):
-            ps_sum_var += (ps_sum_av - ps_sum[l])**2
-        ps_sum_var = np.sqrt(ps_sum_var)
-        ps_sum_var /= (N_SAMPLE-N_min)
         
-        DeltaPS[i] = ps_sum_av
-        SigmaDeltaPS[i] = ps_sum_var
+        ps_phi = []
+        TIME_SPAN = Ts[i]
+        N_min = int(N_SAMPLE*20*ETA/TIME_SPAN) # give it some time to thermalize        
+        DT = DTs[i]
+        str = 'ps_phi_'+f'{ETA:.6f}'+'_'+f'{TEMP:.6f}'+'_'+f'{DT:.6f}'+'_'+f'{BETA}'+'_'+f'{GAMMA}'+'_'+f'{SIGN}'
 
-    PS += [DeltaPS]
-    PSE += [SigmaDeltaPS]
-    
+        for filename in files:
+            if re.match(str,filename):
+                filepath = os.path.join(path,filename)
+                ps = np.loadtxt(filepath,usecols=range(k_max))
+
+                for j in np.arange(N_min,N_SAMPLE,1):
+                    ps_phi += [DX*np.sum(ps[j][:])/k_max]
+
+        SimNo = len(ps_phi)
+        ps_phi = np.array(ps_phi)
+        PS_av[i] = ps_phi.sum()/SimNo
+        for j in range(SimNo):
+            PS_var[i] += (ps_phi[j]-PS_av[i])**2
+        PS_var[i] = np.sqrt(PS_var[i])/(SimNo-1)
+
+    PS += PS_av
+    PSE += PS_var
+  
 #%%
 # convergence plot
 
@@ -127,7 +111,7 @@ markerList = ['o','s']
 fig, ax = plt.subplots(figsize=(6,4))
 
 ax.set_xlabel(r'$h$',fontsize=18)
-ax.set_ylabel(r'$|\bar{S}/S_3 - 1|$',fontsize=18)
+ax.set_ylabel(r'$|\bar{{\cal S}}/{\cal S}_3 - 1|$',fontsize=18)
 ax.set_xscale('log',base=10)
 ax.set_yscale('log',base=10)
 plt.ylim((2e-4,3e-2))
@@ -141,23 +125,11 @@ ax.set_xticklabels([None, None, None, 0.1, 0.2, None, 0.4, None, 0.6, None])
 h = np.linspace(DTs[0],DTs[-1],100)
 
 for w in range(2):
-    # (42)
-    if w==0:
-        params, cov = curve_fit(f, np.log10(DTs[-5:-1]), np.log10(np.abs(PS[w][-5:-1]/psTh[3]-1)))
-        (a, b) = (params[0], params[1])
-        print(a,b)
 
-    # (43)
-    if w==1:
-        params, cov = curve_fit(f, np.log10(DTs[-4:-1]), np.log10(np.abs(PS[w][-4:-1]/psTh[3]-1)))
-        (a, b) = (params[0], params[1])
-        print(a,b)
+    params, cov = curve_fit(f, np.log10(DTs[-5:-1]), np.log10(np.abs(PS[w][-5:-1]/psTh[3]-1)))
+    (a, b) = (params[0], params[1])
+    print(a,b)
 
-    str =  '_'+f'{ETA:.3f}'+\
-            '_'+f'{TEMP:.2f}'+\
-            '_'+f'{BETA}'+\
-            '_'+f'{GAMMA}'+\
-            '_'+f'{SIGN}'
     if b>0:
         str1 = r'$y=$'+f'{a:.2f}'+r'$x+$'+f'{np.abs(b):.2f}'
     else:
@@ -167,7 +139,7 @@ for w in range(2):
     ax.errorbar(DTs*(1+0.01*w), np.abs(PS[w]/psTh[3]-1), PSE[w]/psTh[3], color=ColorList1[w], fmt=markerList[w], ms=5, markerfacecolor=ColorList1[w], elinewidth=1, capsize=2)
     plt.text(xPos[w],yPos[w],str1,fontsize=16,color=ColorList[w])
 plt.savefig('PS_conv.pdf', bbox_inches='tight')
-plt.show()
+#plt.show()
 
 #%%
 # accuracy plot
@@ -175,7 +147,7 @@ plt.show()
 fig, ax = plt.subplots(figsize=(6,4))
 
 ax.set_xlabel(r'$h$',fontsize=18)
-ax.set_ylabel(r'$\bar{S}/S_0 - 1$',fontsize=18)
+ax.set_ylabel(r'$\bar{{\cal S}}/{\cal S}_0 - 1$',fontsize=18)
 ax.set_xscale('log',base=10)
 plt.ylim((-0.108,-0.078))
 plt.xlim((6e-2,8e-1))
@@ -190,11 +162,14 @@ plt.hlines(psTh[3]/psTh[0]-1,6e-2,8e-1,'black',linestyle='dotted')
 ax.set_xticks([0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7])
 ax.set_xticklabels([None, None, None, 0.1, 0.2, None, 0.4, None, 0.6, None])
 
-e1 = ax.errorbar(DTs, PS[0]/psTh[0]-1, PSE[0]/psTh[0], label=r'$(2,4)$', color='blue', fmt='s', ms=5, markerfacecolor='blue', elinewidth=1, capsize=2)
-e2 = ax.errorbar(DTs*1.02, PS[1]/psTh[0]-1, PSE[1]/psTh[0], label=r'$(3,4)$', color='red', fmt='o', ms=5, markerfacecolor='red', elinewidth=1, capsize=2)
+e1 = ax.errorbar(DTs, PS[0]/psTh[0]-1, PSE[0]/psTh[0], label=r'$(2,4)$', color='blue', fmt='o', ms=5, markerfacecolor='blue', elinewidth=1, capsize=2)
+e2 = ax.errorbar(DTs*1.02, PS[1]/psTh[0]-1, PSE[1]/psTh[0], label=r'$(3,4)$', color='red', fmt='s', ms=5, markerfacecolor='red', elinewidth=1, capsize=2)
+plt.text(0.53,-0.093,r'$\textit{1-loop}$',fontsize=14)
+plt.text(0.53,-0.084,r'$\textit{2-loop}$',fontsize=14)
+plt.text(0.53,-0.0868,r'$\textit{3-loop}$',fontsize=14)
 
 ax.legend(handles=[e1, e2], loc='lower left',title=r'$(\gamma,s)$',alignment='left',fontsize=14,title_fontsize=14,markerfirst=False)
 plt.savefig('PS_acc.pdf', bbox_inches='tight')
-plt.show()
+#plt.show()
 
 # %%
